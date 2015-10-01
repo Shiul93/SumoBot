903ef29d6b25a055ab32b1905e579fa767380080
Shiul93 <lfllamas93@gmail.com>
2015-10-01 13:09:30 +0200

    commit

--- Sumo.ino
+++ Sumo.ino
@@ -4,8 +4,14 @@
 #define IRL A1
 #define IRR A2
 
-#define ML 10
-#define MR 11
+#define MRR 10
+#define MRF 11
+#define MLR 5
+#define MLF 6
+
+#define turnT90  1850
+#define turnT180 4060
+#define turnT360 7770
 
 #define model 1080
 
@@ -33,8 +39,10 @@ void setup(){
 
 
 void loop(){
-  
-  sumo();
+ if (Serial.available()){
+   turnSerial();
+ }
+ 
   
 }
 
@@ -46,7 +54,7 @@ void printDistance(){
   Serial.print("   R ="+String(d2));
   Serial.println("   L ="+String(d3));
   Serial.print("\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  delay(100);
+  delay(1000);
 }
 
 void sumo(){
@@ -54,7 +62,7 @@ void sumo(){
   int dR = sR.distance();
   int dL = sL.distance();
 
-  if ((dF<dL)&&(dF<dL)){
+  if (((dF<dL)&&(dF<dL))||(dF<50)){
     front();
   }
   else if (dL<dR){
@@ -65,14 +73,83 @@ void sumo(){
 }
 
 void front(){
-  digitalWrite(MR,HIGH);
-  digitalWrite(ML,HIGH);
+  digitalWrite(MRR,LOW);
+  digitalWrite(MLR,LOW);
+  digitalWrite(MRF,HIGH);
+  digitalWrite(MLF,HIGH);
+  Serial.println("^");
+  
+}
+
+void back(){
+  digitalWrite(MRF,LOW);
+  digitalWrite(MLF,LOW);
+  digitalWrite(MRR,HIGH);
+  digitalWrite(MLR,HIGH);
+  Serial.println("V");
+  
+}
+
+void right(){
+  digitalWrite(MLR,LOW);
+  digitalWrite(MRF,LOW);
+  digitalWrite(MRR,HIGH);
+  digitalWrite(MLF,HIGH);
+  Serial.println("<");
 }
 void left(){
-  digitalWrite(MR,LOW);
-  digitalWrite(ML,HIGH);
+  digitalWrite(MRR,LOW);
+  digitalWrite(MLF,LOW);
+  digitalWrite(MLR,HIGH);
+  digitalWrite(MRF,HIGH);
+  Serial.println(">");
 }
-void right(){
-  digitalWrite(MR,HIGH);
-  digitalWrite(ML,LOW);
+void stop(){
+  digitalWrite(MRR,LOW);
+  digitalWrite(MLF,LOW);
+  digitalWrite(MLR,LOW);
+  digitalWrite(MRF,LOW);
+  Serial.println("+");
 }
+void rotate(int degrees,int direction){
+
+  int rotationTime = map(degrees,0,360,0,turnT360);
+  if (direction = 0){
+    left(); 
+  }else {
+    right();
+  }
+  delay(rotationTime);
+  stop();
+}
+
+
+void turnSerial(){
+  int i=0;
+  char commandbuffer[100];
+  while( Serial.available() && i< 99) {
+        commandbuffer[i++] = Serial.read();
+     }
+     commandbuffer[i++]='\0';
+
+     
+  int turnValue = int(commandbuffer);
+  Serial.println((char*) commandbuffer);
+  if (turnValue<0){
+    rotate(abs(turnValue),0);
+  }else{
+    rotate(abs(turnValue),1);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
